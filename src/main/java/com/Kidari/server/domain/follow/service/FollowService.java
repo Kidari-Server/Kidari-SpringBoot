968bab5dc722247ce26c2167292d1d50742f0ae3
seohyun-lee <applemint2003@gmail.com>
2024-05-01 12:46:41 +0900

    ✨ Member 기늘 구현 위해 FollowService 추가

---
+++ src/main/java/com/Kidari/server/domain/follow/service/FollowService.java
@@ -0,0 +1,8 @@
+package com.Kidari.server.domain.follow.service;
+
+import com.Kidari.server.domain.member.entity.Member;
+
+public class FollowService {
+    public boolean followStatus(Member member) {
+    }
+}
